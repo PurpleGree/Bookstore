43a4742a0fcbf4474fa3c099e66e9170763d40da
Ikeoluwa Oladele <oladelesamson156@gmail.com>
2024-09-29 20:38:44 -0400

    Change font size of Information in the footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,88 @@
+<footer>
+    <hr>
+    <div class="flex p-5  gap-x-3">
+        <div class="flex-grow flex-col flex gap-y-2.5">
+            <x-title />
+            <p class="text-xs text-gray-500">This is an online painting store</p>
+
+            <div class="space-x-3">
+                <a href="http://facebook.com/" class='active'><i class="fa-brands fa-facebook"
+                        style="color: #63E6BE;"></i></a>
+                <a href="#"><i class="fa-brands fa-square-x-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="#"><i class="icon fa fa-rss"></i></a>
+                <a href="#"><i class="fa-brands fa-square-pinterest"></i></a>
+            </div>
+        </div>
+
+        <div class="flex-grow">
+
+            <div>
+                <table>
+                    <thead >
+                        <tr>
+                            <th >
+                                ART GALLERY OPENING TIME
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody >
+                        <tr class="border-b-[1px]  ">
+                            <td class="text-xs font-semibold py-2 text-gray-500">Monday-Friday:</td>
+                            <td class="text-xs font-semibold py-2 text-gray-500 ">08.00 To 18.00</td>
+                        </tr>
+                        <tr class="border-b-[1px]">
+                            <td class="text-xs font-semibold py-2 text-gray-500">Saturday:</td>
+                            <td class="text-xs font-semibold py-2 text-gray-500 ">09.00 To 20.00</td>
+                        </tr>
+                        <tr class="border-b-[1px]">
+                            <td class="text-xs font-semibold py-2 text-gray-500">Sunday:</td>
+                            <td class="text-xs font-semibold py-2 text-gray-500 ">10.00 To 20.00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="flex-grow">
+            <p class="font-semibold mb-2">INFORMATION</p>
+            <ul class="space-y-3">
+                <li class="flex justify-center">
+                    <div >
+                        <span class="icon fa-stack fa-lg">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                          </span>
+                    </div>
+                    <div class="flex-grow">
+                        <p class="text-gray-500 text-sm">Ottawa, Canada</p>
+                    </div>
+                </li>
+
+                <li class="flex justify-center">
+                    <div >
+                        <span class="icon fa-stack fa-lg">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                          </span>
+                    </div>
+                    <div class="flex-grow">
+                        <p class="text-gray-500 text-sm">(613) 0000000 <br> (613) 00000000</p>
+                    </div>
+                </li>
+
+                <li class="flex justify-center">
+                    <div >
+                        <span class="icon fa-stack fa-lg">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                        </span>
+                    </div>
+                    <div class="flex-grow">
+                        <p><a href="#"  class="text-gray-500 text-sm">info@algonquincollege.com</a></p>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+</footer>
